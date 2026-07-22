a9ffaa25c679501a1eb78f4689afb4438c4515e9
Rok-dotcom <kolirameshshivaji@gmail.com>
2026-07-22 10:00:16 +0530

    fixed header footer on other page

--- Front-End/Bus-Tracker/src/App.jsx
+++ Front-End/Bus-Tracker/src/App.jsx
@@ -1,27 +1,33 @@
 import { Routes, Route } from "react-router-dom";
 import AppHeader from "./components/AppHeader";
 import AppBody from "./components/AppBody";
-import IntputForm from "./components/InputForm";
+import InputForm from "./components/InputForm";
 import Waiting from "./components/Waiting";
 import InBus from "./components/InBus";
 import NewHeader from "./components/NewHeader";
 import UserGuide from "./components/UserGuide";
 import AppFooter from "./components/AppFooter";
 
+function Home() {
+  return (
+    <>
+      <NewHeader />
+      <UserGuide />
+      <AppBody />
+      <AppFooter />
+    </>
+  );
+}
+
 function App() {
   return (
     <>
-      <NewHeader></NewHeader>
-      <UserGuide></UserGuide>
-
       <Routes>
-        <Route path="/" element={<AppBody />} />
-        <Route path="/InputForm" element={<IntputForm />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/InputForm" element={<InputForm />} />
         <Route path="/Waiting" element={<Waiting />} />
         <Route path="/InBus" element={<InBus />} />
       </Routes>
-
-      <AppFooter></AppFooter>
     </>
   );
 }
